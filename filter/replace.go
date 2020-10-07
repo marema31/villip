7be0e506d072a19ec0dd98b58b69ce42a54d92a7
thibaud.hamet <thibaud.hamet@corp.ovh.com>
2020-10-07 18:08:40 +0200

    refactor replace body and read body in one func

--- filter/replace.go
+++ filter/replace.go
@@ -39,7 +39,7 @@ func (f *Filter) headerReplace(log *logrus.Entry, parsedHeader http.Header, head
 	log.Debug("Checking if need to replace header")
 
 	for _, h := range headerConfig {
-		if parsedHeader[h.Name] == nil || parsedHeader.Get(h.Name) == "" || h.Force {
+		if parsedHeader.Get(h.Name) == "" || h.Force {
 			parsedHeader.Set(h.Name, h.Value)
 			log.Debug(fmt.Sprintf("Set header %s with value :  %s", h.Name, h.Value))
 		}
@@ -91,4 +91,53 @@ func (f *Filter) readBody(bod io.ReadCloser, h http.Header) (string, error) {
 	}
 
 	return string(b), err
+}
+
+func (f *Filter) readAndReplaceBody(requestURL string, rep []replaceParameters, bod io.ReadCloser, parsedHeader http.Header) (int, io.ReadCloser, string, string, error) {
+	var originalBody string
+
+	var modifiedBody string
+
+	var contentLength int
+
+	var body io.ReadCloser
+
+	switch parsedHeader.Get("Content-Encoding") {
+	case "gzip":
+		body, _ = gzip.NewReader(bod)
+		//		defer body.Close()
+	default:
+		body = bod
+	}
+
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		return 0, nil, "", "", err
+	}
+
+	originalBody = string(b)
+
+	f.log.Debug(fmt.Sprintf("Body before the replacement : %s", originalBody))
+
+	modifiedBody = do(requestURL, originalBody, rep)
+
+	f.log.Debug(fmt.Sprintf("Body after the replacement : %s", modifiedBody))
+
+	switch parsedHeader.Get("Content-Encoding") {
+	case "gzip":
+		w, err := f.compress(modifiedBody)
+		if err != nil {
+			return 0, nil, "", "", err
+		}
+
+		body = ioutil.NopCloser(w)
+		contentLength = w.Len()
+
+	default:
+		buf := bytes.NewBufferString(modifiedBody)
+		body = ioutil.NopCloser(buf)
+		contentLength = buf.Len()
+	}
+
+	return contentLength, body, originalBody, modifiedBody, nil
 }
