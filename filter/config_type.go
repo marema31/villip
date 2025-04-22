76886bcb1a2d826a93fa9dd9edaaf8703c7c831b
Marc Carmier <marema31@users.noreply.github.com>
2025-04-22 06:08:27 +0200

    feat: add configuration for CRD generation (#27)

--- filter/config_type.go
+++ filter/config_type.go
@@ -1,41 +1,46 @@
 package filter
 
-// Creplacement configuration parts for replacement.
+// Configuration  for replacement.
 type Creplacement struct {
-	From string   `yaml:"from" json:"from,omitempty"`
-	To   string   `yaml:"to" json:"to,omitempty"`
+	From string `yaml:"from" json:"from,omitempty"`
+	To   string `yaml:"to" json:"to,omitempty"`
+	// +kubebuilder:validation:Optional
 	Urls []string `yaml:"urls" json:"urls,omitempty"`
 }
 
-// Cdump configuration parts for dump log.
+// Configuration for dump log.
 type Cdump struct {
-	Folder string   `yaml:"folder" json:"folder,omitempty"`
-	URLs   []string `yaml:"urls" json:"urls,omitempty"`
+	Folder string `yaml:"folder" json:"folder,omitempty"`
+	// +kubebuilder:validation:Optional
+	URLs []string `yaml:"urls" json:"urls,omitempty"`
 }
 
-// Cheader configuration parts for header management.
+// Configuration for header management.
 type Cheader struct {
 	Name  string `yaml:"name" json:"name,omitempty"`
 	Value string `yaml:"value" json:"value,omitempty"`
 	Force bool   `yaml:"force" json:"force,omitempty"`
-	Add   bool   `yaml:"add" json:"add,omitempty"`
-	UUID  bool   `yaml:"uuid" json:"uuid,omitempty"`
+	// +kubebuilder:default=false
+	Add bool `yaml:"add" json:"add,omitempty"`
+	// +kubebuilder:validation:Optional
+	UUID bool `yaml:"uuid" json:"uuid,omitempty"`
 }
 
-// Caction configuration parts for request and response  management.
+// Configuration for request and response  management.
 type Caction struct {
 	Replace []Creplacement `yaml:"replace" json:"replace,omitempty"`
 	Header  []Cheader      `yaml:"header" json:"header,omitempty"`
 }
 
-// CtokenAction configuration parts for token management.
+// Configuration for token management.
 type CtokenAction struct {
 	Header string `yaml:"header" json:"header,omitempty"`
 	Value  string `yaml:"value" json:"value,omitempty"`
+	// +kubebuilder:validation:Enum=accept,reject,notEmpty
 	Action string `yaml:"action" json:"action,omitempty"`
 }
 
-// Config common structure for configuration regardless the source.
+// Rule configuration.
 type Config struct {
 	ContentTypes []string       `yaml:"content-types" json:"content-types,omitempty"` //nolint: tagliatelle
 	Dump         Cdump          `yaml:"dump" json:"dump,omitempty"`
